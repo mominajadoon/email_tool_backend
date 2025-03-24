c3b9abdcfe234e2a3fc95daa4c6e8bf1fa747f31
Momina <mominajadoon77@gmail.com>
2025-03-25 04:59:09 +0500

    Added vercel.json for deployment

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-// Import Auth Routes
-const authRoutes = require("../backend/Routes/authRoutes");
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 
-// Base Route for Auth
-app.use("/api/auth", authRoutes);
+// Routes
+app.use("/api/auth", require("../backend/Routes/authRoutes"));
+// app.use("/api/email", require("./Routes/emailRoutes"));
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Home Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
-// updated
 
+// Export App
+module.exports = app;
